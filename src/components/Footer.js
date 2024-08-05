5cd50464c07b6c930780d7c283a21353cf971128
GabrielItaqui <105838615+GabrielItaqui@users.noreply.github.com>
2024-08-05 18:42:59 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ const Footer = () => {
       <div className="footer-content">
         <p>Conecte-se conosco:</p>
         <div className="social-links">
-          <a href="https://www.facebook.com/anhanguera" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+          <a href="https://www.facebook.com/caxias.anhanguera/?locale=pt_BR" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
             <i className="fab fa-facebook-f"></i>
           </a>
-          <a href="https://www.twitter.com/anhanguera" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+          <a href="https://x.com/anhanguera_edu" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
             <i className="fab fa-twitter"></i>
           </a>
-          <a href="https://www.instagram.com/anhanguera" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+          <a href="https://www.instagram.com/anhanguera.caxiasdosul/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
             <i className="fab fa-instagram"></i>
           </a>
-          <a href="https://www.linkedin.com/school/anhanguera" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+          <a href="https://www.linkedin.com/school/anhanguera-educacional-sa/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
             <i className="fab fa-linkedin-in"></i>
           </a>
         </div>
